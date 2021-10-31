d7007dba01fd6276fada55bd38a759ea6963af29
zjria <zakaria.com.bd@gmail.com>
2021-10-31 22:22:48 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { ButtonGroup, Button } from 'react-bootstrap';
+import Home from './component/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Allahu sorbo soktiman</h1>       
+        <br></br>
+        <Home></Home>
+
     </div>
   );
 }
